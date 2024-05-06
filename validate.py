8c33481ec385f7c70ddf33e0863d0835e3f7af99
Felipe Ferrari <felferrari@gmail.com>
2024-05-06 11:51:38 -0300

    up

--- validate.py
+++ validate.py
@@ -3,12 +3,9 @@ from pathlib import Path
 from rioxarray import open_rasterio
 import numpy as np
 import geopandas as gpd
-from tqdm import tqdm
 import matplotlib.pyplot as plt
 from shutil import rmtree
-from shapely.geometry import box
 from rasterio.enums import Resampling
-from skimage.measure import block_reduce
 import pandas as pd
 
 parser = argparse.ArgumentParser()
@@ -72,10 +69,10 @@ def main():
     pred_gpd.to_file(output_file, layer = 'predictions', driver="GPKG")
     ref_gpd.to_file(output_file, layer = 'reference', driver="GPKG")
     
-    predictions.values[predictions.values < 0.01] = 0
+    #predictions.values[predictions.values < 0.01] = 0
     
     absolute_error = predictions - reference
-    relative_error = absolute_error / reference
+    relative_error = np.abs(absolute_error) / reference
     
     mask_ = np.repeat(mask, 48, axis=0)
     
@@ -85,21 +82,108 @@ def main():
     absolute_error.rio.to_raster(output_figures / 'absolute_error.tif')
     relative_error.rio.to_raster(output_figures / 'relative_error.tif')
     
-    similarity = np.abs(relative_error * 100) < threshold
+    similarity = (relative_error * 100) < threshold
     similarity.values = similarity.values.astype(np.float32)
-    similarity.rio.to_raster(output_figures / 'similarity.tif')
-    
-    #Consider similarity = 0 when predictions ==0 and reference == 0
-    
     similarity.values[mask_.values == 0] = np.nan
     similarity.rio.write_nodata(np.nan, inplace = True)
+    similarity.rio.to_raster(output_figures / f'similarity_th{threshold}.tif')
+    
+    #Consider similarity = 0 when predictions ==0 and reference == 0
     similarity_zeroin = similarity.copy()
     similarity_zeroin.values[np.logical_and(predictions.values ==0, reference.values == 0)] = 1
+    similarity_zeroin.values[mask_.values == 0] = np.nan
     similarity_zeroin.rio.write_nodata(np.nan, inplace = True)
-    similarity_zeroin.rio.to_raster(output_figures / 'similarity_zeroin.tif')
+    similarity_zeroin.rio.to_raster(output_figures / f'similarity_zeroin_th{threshold}.tif')
+    
+    
+    corrected_relative_error = relative_error.copy().values
+    corrected_relative_error[np.logical_and(predictions.values ==0, reference.values == 0)] = 0
+    corrected_relative_error = corrected_relative_error.flatten()
+    corrected_relative_error = corrected_relative_error[mask_.values.flatten() == 1]
+    corrected_relative_error = corrected_relative_error[corrected_relative_error != np.inf]
+    corrected_relative_error = 100*corrected_relative_error
+    corrected_relative_error = np.clip(corrected_relative_error, 0, 1000)
+    
+    fig, ax = plt.subplots()
+    plt.hist(corrected_relative_error, bins = 50, log=True)
+    plt.xlabel('%')
+    plt.ylabel('Count')
+    fig.savefig(output_figures / f'relative_errors_hist_0.png')
+    plt.close(fig)
+    
+    corrected_relative_error = np.clip(corrected_relative_error, 0, 100)
+    fig, ax = plt.subplots()
+    plt.hist(corrected_relative_error, bins = 50, log=True)
+    plt.xlabel('%')
+    plt.ylabel('Count')
+    fig.savefig(output_figures / f'relative_errors_hist_1.png')
+    plt.close(fig)
+    
+    corrected_relative_error = np.clip(corrected_relative_error, 0, 10)
+    fig, ax = plt.subplots()
+    plt.hist(corrected_relative_error, bins = 50, log=True)
+    plt.xlabel('%')
+    plt.ylabel('Count')
+    fig.savefig(output_figures / f'relative_errors_hist_2.png')
+    plt.close(fig)
+    
+    corrected_relative_error = np.clip(corrected_relative_error, 0, 1)
+    fig, ax = plt.subplots()
+    plt.hist(corrected_relative_error, bins = 50, log=True)
+    plt.xlabel('%')
+    plt.ylabel('Count')
+    fig.savefig(output_figures / f'relative_errors_hist_3.png')
+    plt.close(fig)
+    
+    # priority cells
+    shape = reference.shape[1:]
+    similarity_priority = predictions.copy()
+    similarity_priority.values = np.zeros_like(similarity_priority)
+    pred_order = predictions.copy()
+    pred_order.values = np.zeros_like(pred_order)
+    ref_order = predictions.copy()
+    ref_order.values = np.zeros_like(ref_order)
+    diff_order = predictions.copy()
+    diff_order.values = np.zeros_like(diff_order)
+    for biweek in range(predictions.shape[0]):
+        pred_bw = predictions.values[biweek].flatten()
+        ref_bw = reference.values[biweek].flatten()
+        
+        pred_order_bw = np.zeros_like(pred_bw)
+        ref_order_bw = np.zeros_like(ref_bw)
+        
+        pred_order_bw[np.argsort(pred_bw)[-100:]] = (np.arange(100, 0, -1) - 1)
+        ref_order_bw[np.argsort(ref_bw)[-100:]] =  (np.arange(100, 0, -1) - 1)
+        
+        diff_bw = np.abs(pred_order_bw - ref_order_bw)
+        
+        diff_order.values[biweek] = diff_bw.reshape(shape)
+        ref_order.values[biweek] = ref_order_bw.reshape(shape)
+        pred_order.values[biweek] = pred_order_bw.reshape(shape)
+        
+        diff_bw = (diff_bw <= 10).astype(np.float32).reshape(shape)
+        similarity_priority.values[biweek] = diff_bw
+        
+    similarity_priority.values[mask_.values == 0] = np.nan
+    similarity_priority.rio.write_nodata(np.nan, inplace = True)
+    similarity_priority.rio.to_raster(output_figures / f'similarity_priority.tif')
+    
+    diff_order.values[mask_.values == 0] = np.nan
+    diff_order.rio.write_nodata(np.nan, inplace = True)
+    diff_order.rio.to_raster(output_figures / f'diff_order.tif')
+    
+    ref_order.values[mask_.values == 0] = np.nan
+    ref_order.rio.write_nodata(np.nan, inplace = True)
+    ref_order.rio.to_raster(output_figures / f'ref_order.tif')
+    
+    pred_order.values[mask_.values == 0] = np.nan
+    pred_order.rio.write_nodata(np.nan, inplace = True)
+    pred_order.rio.to_raster(output_figures / f'pred_order.tif')
+    
     
     means = []
     means_nonzeros = []
+    means_priority = []
     
     for downscale_factor in range(1, max_cells+ 1):
         
@@ -123,6 +207,16 @@ def main():
         
         means_nonzeros.append(100*np.nanmean(similarity_sampled, axis=(1,2)))
         
+        similarity_sampled = similarity_priority.rio.reproject(
+            similarity_priority.rio.crs,
+            resolution=(downscale_factor*cell_size, downscale_factor*cell_size),
+            resampling=Resampling.average,
+        )
+
+        similarity_sampled.rio.to_raster(output_figures / f'similarity_priority_r{downscale_factor}.tif')
+        
+        means_priority.append(100*np.nanmean(similarity_sampled, axis=(1,2)))
+        
     means = np.stack(means)
     df = pd.DataFrame(
         data = means
@@ -134,6 +228,15 @@ def main():
         data = means_nonzeros
     )
     df.to_excel(output_figures / 'results_nonzeros.xlsx')
+    
+    means_priority = np.stack(means_priority)
+    df = pd.DataFrame(
+        data = means_priority
+    )
+    df.to_excel(output_figures / 'results_priority.xlsx')
+    
+    
+    
     
 if __name__ == '__main__':
     
